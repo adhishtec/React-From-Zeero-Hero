5eaf1963387f9bceccb1e3fc99d9a4aab9829f7a
Adhish Nigam <mradhishnigam@gmail.com>
2023-09-18 11:53:17 +0200

    My first comit

---
+++ App.js
@@ -0,0 +1,4 @@
+const getRootData = React.createElement('h1',{},"hello I am React")
+getRootData.innerHTML="Adhish"
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(getRootData)
